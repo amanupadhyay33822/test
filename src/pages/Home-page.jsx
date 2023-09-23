21b993956eadcb01171d773045a3ea038af12d1a
onlyVishesh <Visheshmaurya14@gmail.com>
2023-09-23 19:59:35 +0530

    Only home page left

--- src/pages/Home-page.jsx
+++ src/pages/Home-page.jsx
@@ -46,7 +46,7 @@ const Home = () =>{
     return(
         <>
         <Navbar/>
-        <section>
+        <section style={{marginTop:'9vh'}}>
     
 {/* <!-- nav bar ends here --> */}
 {/* <!-- first section --> */}
