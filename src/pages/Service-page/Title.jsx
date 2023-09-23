12b1ade9d9c82025b626c24f5d93bbbe0ab0bb1e
onlyVishesh <Visheshmaurya14@gmail.com>
2023-09-23 17:23:30 +0530

    Service page fixed

--- src/pages/Service-page/Title.jsx
+++ src/pages/Service-page/Title.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import "./Title.css"
 
-import { Parallax } from 'react-parallax';
-
-
 const Title = () => {
     return (
-        <section className="title box hidden" style={{
-            paddingLeft:'0vh',
-            paddingRight:'0vh',
-            marginTop:'7.5vh'
-        }}>
-            <Parallax strength={300} bgImage='./images/Service-images/service page first section.webp' className='para'>
-            <div>
-            <h1 style={{
-               
-            }}><abbr title="MEDICAL means AMBUvians">MEDICAL means AMBUvians</abbr></h1>
+        <section className="title1 box hidden" style={{marginTop:"10vh"}}>
+            <h1><abbr title="MEDICAL means AMBUvians">MEDICAL means AMBUvians</abbr></h1>
             <p>We will try to provide the best, fastest, and safest ambulance service all over INDIA</p>
-            </div>
-            </Parallax>
-           
-            {/* <figure className="ambulance">
-                <img loading="lazy" src="./images/Service-images/service page first section.webp" alt="cover image" style={{ width: '100vw' }} />
-            </figure> */}
+            <figure className="ambulance">
+                <img loading="lazy" src="./images/Service-images/service page first section.webp" alt="cover" style={{ width: '100vw' }} />
+            </figure>
         </section>
     );
 };
