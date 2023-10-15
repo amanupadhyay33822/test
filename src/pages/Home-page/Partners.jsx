cc00ffbad1a93e654a4391ded04965cf4af6f3bf
onlyVishesh <Visheshmaurya14@gmail.com>
2023-10-15 21:28:25 +0530

    Added new partners

--- src/pages/Home-page/Partners.jsx
+++ src/pages/Home-page/Partners.jsx
@@ -4,6 +4,7 @@ import "./Partners.css"
 
 import teific_logo from "../../assets/Home-images/teific.png";
 
+
 function Partners() {
   return (
     <>
@@ -11,13 +12,18 @@ function Partners() {
         <a>Our Partners</a>
       </div>
       <div className="partners">
-      <a href="https://www.teific.in" className="side-partner">
-          <img loader="lazy" src={teific_logo} alt="" />
-          <figcaption>Teific</figcaption>
+      <a href="/" className="side-partner">
+          <img loader="lazy" src="./images/Home-images/school.jpeg" alt="" />
+          <figcaption>St. R. C. Convent School</figcaption>
         </a>
-        <a href="hhttps://www.kiet.edu/">
+        <a href="https://www.kiet.edu/">
           <img loader="lazy" src="./images/Home-images/kiet.png" alt="" />
           <figcaption>KIET Group Of Institutions</figcaption>
+
+        </a>
+        <a href="/">
+          <img loader="lazy" src="./images/Home-images/club.png" alt="" />
+          <figcaption>Scoieo-Tech Innovation Club</figcaption>
 
         </a>
         
